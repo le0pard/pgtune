7adf37a7ff2e317b917b3d3a1f7b8e7d927d2b37
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-08-10 19:19:37 +0300

    update js libs

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require ./vendors/modernizr
-//= require ./vendors/jquery-2.1.0.min
+//= require ./vendors/jquery-2.1.1.min
 //= require ./vendors/fastclick
 
 //= require ./vendors/foundation/foundation
