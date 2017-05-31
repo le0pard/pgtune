e2adb21c257671d383383e3e28a4eb30461cad07
Alexey Vasiliev <leopard.not.a@gmail.com>
2017-05-31 16:48:11 +0300

    minify assets

--- config.rb
+++ config.rb
@@ -42,9 +42,9 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  #activate :minify_css
+  activate :minify_css
   # Minify Javascript on build
-  #activate :minify_javascript
+  activate :minify_javascript
   # asset hash
   activate :asset_hash
   # min html
