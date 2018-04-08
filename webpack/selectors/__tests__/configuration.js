c78a400471041383500dc174249bfb8f9b675633
Alexey Vasiliev <leopard.not.a@gmail.com>
2018-04-08 13:41:35 +0300

    add tests

---
+++ webpack/selectors/__tests__/configuration.js
@@ -0,0 +1,36 @@
+import {
+  isReadyForConfiguration,
+  maxConnections
+} from '../configuration'
+
+describe('isReadyForConfiguration', () => {
+  it('nothing set', () => {
+    expect(isReadyForConfiguration({
+      configuration: {}
+    })).toEqual(false)
+  })
+  it('set totalMemory', () => {
+    expect(isReadyForConfiguration({
+      configuration: {
+        totalMemory: 100
+      }
+    })).toEqual(true)
+  })
+})
+
+describe('maxConnections', () => {
+  it('web app', () => {
+    expect(maxConnections({
+      configuration: {
+        dbType: 'web'
+      }
+    })).toEqual(200)
+  })
+  it('oltp app', () => {
+    expect(maxConnections({
+      configuration: {
+        dbType: 'oltp'
+      }
+    })).toEqual(300)
+  })
+})
