ac9dc55fab82e4fa8b3945561d4bdcceabe29cb7
Alexey Vasiliev <leopard.not.a@gmail.com>
2018-05-10 21:38:24 +0300

    add svg logo

--- webpack/pages/dashboard/index.jsx
+++ webpack/pages/dashboard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MainGenerator from 'containers/mainGenerator'
+import pgtuneLogo from './pgtune.svg'
 
 import './dashboard.sass'
 
@@ -7,7 +8,18 @@ export default class DashboardPage extends React.Component {
   render() {
     return (
       <div className="dashboard-page">
-        <h1 className="dashboard-page-title">PGTune</h1>
+        <div className="dashboard-header-wrapper">
+          <div className="dashboard-header-logo">
+            <img alt="Pgtune"
+              className="dashboard-header-logo__svg"
+              src={pgtuneLogo} />
+          </div>
+          <div className="dashboard-header-title">
+            <h1 className="dashboard-header-title__text">
+              PGTune
+            </h1>
+          </div>
+        </div>
         <MainGenerator />
       </div>
     )
