545e9c35207eb1f960749ab4ee69a8a7869db21f
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-11-23 14:11:20 +0200

    update tooltips

--- webpack/components/form/tooltip.jsx
+++ webpack/components/form/tooltip.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import RcTooltip from 'rc-tooltip'
+import classnames from 'classnames'
 
 import './tooltip.sass'
 
@@ -16,6 +16,43 @@ export default class Tooltip extends React.Component {
     className: PropTypes.string
   }
 
+  constructor(props) {
+    super(props)
+
+    this.closeTooltip = this.closeTooltip.bind(this)
+    this.tooltipRef = React.createRef()
+
+    this.state = {
+      isVisible: false
+    }
+  }
+
+  componentDidMount() {
+    document.body.addEventListener('click', this.closeTooltip)
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('click', this.closeTooltip)
+  }
+
+  closeTooltip(e) {
+    if (
+      this.tooltipRef.current &&
+      !this.tooltipRef.current.contains(e.target) &&
+      this.state.isVisible
+    ) {
+      this.setState(() => ({
+        isVisible: false
+      }))
+    }
+  }
+
+  toggleTooltip() {
+    this.setState((prevState) => ({
+      isVisible: !prevState.isVisible
+    }))
+  }
+
   render() {
     const {
       id,
@@ -24,19 +61,25 @@ export default class Tooltip extends React.Component {
       className
     } = this.props
 
+    const {isVisible} = this.state
+
     return (
-      <RcTooltip
-        id={id}
-        placement="topRight"
-        trigger={['click']}
-        overlay={text}
-      >
-        <div className={className}>
-          <a id={`${id}Content`} className={className && `${className}__link`} href="#">
-            {label}
-          </a>
-        </div>
-      </RcTooltip>
+      <div ref={this.tooltipRef} className={classnames('tooltip-container', className)}>
+        <div className={classnames('tooltip-block', {
+          ['tooltip-block__visible']: isVisible,
+          ['tooltip-block__hidden']: !isVisible
+        })}>{text}</div>
+        <button
+          id={`${id}Content`}
+          className={classnames('tooltip-button', {
+            [`${className}__button`]: !!className
+          })}
+          type="button"
+          onClick={this.toggleTooltip.bind(this)}
+        >
+          {label}
+        </button>
+      </div>
     )
   }
 }
--- webpack/init.js
+++ webpack/init.js
@@ -4,3 +4,5 @@ import './css/app.sass'
 // general polifils
 import 'whatwg-fetch'
 import 'promise-polyfill/dist/polyfill'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
