416ffd1f63c58e360f47f2c37bc8f423d25f29c2
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-09-30 19:34:29 +0300

    pg 14 added

--- webpack/selectors/configuration.js
+++ webpack/selectors/configuration.js
@@ -65,6 +65,11 @@ const getDbDefaultValues = createSelector(
         ['max_worker_processes']: 8,
         ['max_parallel_workers_per_gather']: 2,
         ['max_parallel_workers']: 8
+      },
+      14: {
+        ['max_worker_processes']: 8,
+        ['max_parallel_workers_per_gather']: 2,
+        ['max_parallel_workers']: 8
       }
     }[dbVersion]
   )
