fe60bcc37dc8a59c9586b2360fba3ac82c2b4eb3
Andrew Berezovskyi <andrew@berezovskyi.me>
2021-12-06 11:45:38 +0100

    Fix up English
    "optimally configure the database can only be given all these parameters" doesn't make sense grammatically.

--- webpack/pages/about/index.js
+++ webpack/pages/about/index.js
@@ -7,7 +7,7 @@ const AboutPage = () => (
   <div className="about-page">
     <img className="about-page-logo" src={Logo} alt="PGTune" />
     <p>
-      <strong>PGTune</strong> calculate configuration for PostgreSQL based on the maximum performance for a given hardware configuration. It isn't a <a href="https://en.wikipedia.org/wiki/No_Silver_Bullet">silver bullet</a> for the optimization settings of PostgreSQL. Many settings depend not only on the hardware configuration, but also on the size of the database, the number of clients and the complexity of queries, so that optimally configure the database can only be given all these parameters.
+      <strong>PGTune</strong> calculate configuration for PostgreSQL based on the maximum performance for a given hardware configuration. It isn't a <a href="https://en.wikipedia.org/wiki/No_Silver_Bullet">silver bullet</a> for the optimization settings of PostgreSQL. Many settings depend not only on the hardware configuration, but also on the size of the database, the number of clients and the complexity of queries. An optimal configuration of the database can only be made given all these parameters are taken into account.
     </p>
     <h3>Useful links</h3>
     <ul className="about-page-list">
