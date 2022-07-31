332801b1979b04f1a1876e1821a72863209179c7
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-07-31 23:07:45 +0300

    new jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  modulePaths: ['<rootDir>/assets'],
   moduleDirectories: ['node_modules', '<rootDir>/assets'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/vendor/'],
   transform: {
