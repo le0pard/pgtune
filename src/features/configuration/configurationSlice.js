61720b9ee41c28d81b613fe38dfe7531d5b2639b
Alexey Vasiliev <leopard.not.a@gmail.com>
2025-04-30 13:27:23 +0300

    #113 fix work mem calculation

--- src/features/configuration/configurationSlice.js
+++ src/features/configuration/configurationSlice.js
@@ -354,7 +354,7 @@ export const selectWorkMem = createSelector(
       return 1
     })()
     // work_mem is assigned any time a query calls for a sort, or a hash, or any other structure that needs a space allocation, which can happen multiple times per query. So you're better off assuming max_connections * 2 or max_connections * 3 is the amount of RAM that will actually use in reality. At the very least, you need to subtract shared_buffers from the amount you're distributing to connections in work_mem.
-    // The other thing to consider is that there's no reason to run on the edge of available memory. If you do that, there's a very high risk the out-of-memory killer will come along and start killing PostgreSQL backends. Always leave a buffer of some kind in case of spikes in memory usage. So your maximum amount of memory available in work_mem should be (RAM - shared_buffers) / (max_connections + max_worker_processes) * 3)).
+    // The other thing to consider is that there's no reason to run on the edge of available memory. If you do that, there's a very high risk the out-of-memory killer will come along and start killing PostgreSQL backends. Always leave a buffer of some kind in case of spikes in memory usage. So your maximum amount of memory available in work_mem should be (RAM - shared_buffers) / ((max_connections + max_worker_processes) * 3).
     const workMemValue =
       (totalMemoryKb - sharedBuffersValue) / ((maxConnectionsValue + parallelForWorkMem) * 3)
     let workMemResult = {
