5824db9c5143387938b2f58cbd4510d69886c1ba
Alexey Vasiliev <leopard.not.a@gmail.com>
2025-11-29 20:10:47 +0200

    new eslint

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import js from '@eslint/js'
+
+export default defineConfig([
+  globalIgnores(['**/dist', '**/.yarn/**', '**/.pnp.**']),
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    extends: [
+      js.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      reactRefresh.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      eslintPluginPrettierRecommended
+    ],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true
+        }
+      ],
+      'react/jsx-uses-react': 'error'
+    }
+  }
+])
